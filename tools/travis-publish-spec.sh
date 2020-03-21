2d122937937bc2e53e54db8e0e427c96c709ba66
Norman Walsh <ndw@nwalsh.com>
2020-03-21 16:37:03 +0000

    Publish schemas and libraries to the grammar repo

--- tools/travis-publish-spec.sh
+++ tools/travis-publish-spec.sh
@@ -5,47 +5,68 @@ if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
     exit
 fi
 
-if [ "$GIT_PUB_REPO" != "" ]; then
-    echo "Preparing to publish to $GIT_PUB_REPO..."
+if [ "$GH_TOKEN" = "" ]; then
+    echo "No GitHub token, publication is not possible."
+    exit
+fi
+
+if [ "$GIT_PUB_REPO" = "" ]; then
+    echo "No publication repo; publication is not possible."
+    exit
+fi
+
+git config --global user.email ${GIT_EMAIL}
+git config --global user.name ${GIT_NAME}
+
+TIP=${TRAVIS_TAG:="head"}
+
+cd $HOME
+echo "Publishing to gh-pages/${TRAVIS_BRANCH}/${TIP} in ${GIT_PUB_REPO}"
+    
+git clone --quiet --branch=gh-pages \
+    https://${GH_TOKEN}@github.com/${GIT_PUB_REPO} gh-pages > /dev/null
+
+# N.B. The expectation here is that the this repository
+# may be updating gh-pages in a different repository
+# Consequently, we don't try to remove the old files.
+# Occasional manual cleanup may be required.
+
+cd gh-pages
+mkdir -p ./${TRAVIS_BRANCH}/${TIP}
+cp -Rf $TRAVIS_BUILD_DIR/build/dist/* ./${TRAVIS_BRANCH}/${TIP}/
+
+git add --verbose -f .
+git commit -m "Travis build $TRAVIS_BUILD_NUMBER for $GIT_PUB_REPO"
+echo git push -fq origin gh-pages > /dev/null
+
+cd $HOME
+rm -rf gh-pages
+
+echo -e "Published."
+
+if [ "$TRAVIS_REPO_SLUG" = "xproc/3.0-steps" ]; then
+    # This is a merged PR going to the public repo
+    # Publish the updated library and grammar files
+    # to the grammar repo.
+    GRAMMAR_REPO_SLUG=xproc/3.0-grammar
+
     cd $HOME
-    git config --global user.email ${GIT_EMAIL}
-    git config --global user.name ${GIT_NAME}
+    echo "Publishing library and grammar files to ${GRAMMAR_REPO_SLUG}"
+    
+    git clone --quiet --branch=master \
+        https://${GH_TOKEN}@github.com/${GRAMMAR_REPO_SLUG} grammar > /dev/null
 
-    if [ "$GH_TOKEN" != "" ]; then
-        echo "Publishing..."
+    cd grammar
+    mkdir -p ./steps
+    cp -Rf $TRAVIS_BUILD_DIR/build/dist/etc/* ./steps/
+    rm f ./steps/*/source.xml ./steps/*/toc.xml
 
-        git clone --quiet --branch=gh-pages \
-            https://${GH_TOKEN}@github.com/${GIT_PUB_REPO} gh-pages > /dev/null
+    git add --verbose -f .
+    git commit -m "Travis build $TRAVIS_BUILD_NUMBER for $TRAVIS_REPO_SLUG"
+    echo git push -fq origin gh-pages > /dev/null
 
-        TIP=${TRAVIS_TAG:="head"}
+    cd $HOME
+    rm -rf gh-pages
 
-        # N.B. gh-pages here is updated by two different repositories.
-        # Consequently, we don't try to remove the old files.
-        # Occasional manual cleanup may be required.
-
-        cd gh-pages
-        mkdir -p ./${TRAVIS_BRANCH}/${TIP}
-        cp -Rf $TRAVIS_BUILD_DIR/build/dist/* ./${TRAVIS_BRANCH}/${TIP}
-
-        git add --verbose -f .
-        git commit -m "Successful travis build $TRAVIS_BUILD_NUMBER"
-        git push -fq origin gh-pages > /dev/null
-
-        echo -e "Published specification to gh-pages.\n"
-
-        if [ "$TRAVIS_REPO_SLUG" == "xproc/3.0-steps" ]; then
-            # Force a build of the 3.0-grammar repo
-            # in case the steps have updated grammars.
-            cd $HOME
-            git clone --quiet --branch=master \
-                https://${GH_TOKEN}@github.com/xproc/3.0-grammar grammar \
-                > /dev/null
-            cd grammar
-            echo "Grammar for steps from build $TRAVIS_BUILD_NUMBER" \
-                 > src/step-build.txt
-            git add --verbose -f .
-            git commit -m "Steps build $TRAVIS_BUILD_NUMBER"
-            git push -fq origin master > /dev/null
-        fi
-    fi
+    echo -e "Published."
 fi
